46a0969650714a283240c1a84d5ab0b56a768bd6
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-06-29 00:27:01 +0300

    Implement validation for the project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,9 @@
 class Project < ApplicationRecord
+  validates :name, :summary, :start_date, :end_date, presence: true
+  validates :name, length: { maximum: 100 }
+  validates :summary, length: { maximum: 5000 }
+  #validates :start_date, :end_date, length: { is: 10 }
+  validates :start_date, :end_date, format: { with: /\d{2}\/\d{2}\/\d{4}/,
+                                              message: "Date must be in the following format: mm/dd/yyyy" }
+
 end
