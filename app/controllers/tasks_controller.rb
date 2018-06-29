a63e311d5bbd573b460988309fe99d9312d1f8d6
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-06-29 14:05:05 +0300

    Generate controller named Tasks, Add necessary actions

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [ :show, :edit, :update, :destroy ]
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit; end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to @task
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:recipe).permit(:title, :description, :status)
+  end
+end
